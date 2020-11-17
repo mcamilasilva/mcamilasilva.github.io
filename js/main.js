8694b3945acca55cda833a6c2590a8c9f3c652ca
Camila Silva <manoelacamila.silva@gmail.com>
2020-11-16 22:45:28 -0300

    typo / back to top button / linkedin button

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
-// Add your javascript here
-// Don't forget to add it into respective layouts where this js file is needed
+$(document).ready(function () {
 
-$(document).ready(function() {
-  AOS.init( {
+  AOS.init({
     disable: 'mobile'
   }); // initialize animate on scroll library
 
+
+  $(".has-tooltip").tooltip();
+
   // cross-browser soluction for smooth scroll >> https://www.w3schools.com/howto/howto_css_smooth_scroll.asp
-  $("a.scroll-behavior-smooth").on('click', function(event) {
+  $("a.scroll-behavior-smooth").on('click', function (event) {
 
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
@@ -21,11 +22,27 @@ $(document).ready(function() {
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 800, function(){
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
+      }, 800, function () {
       });
     } // End if
+  });
+
+  $('#back-to-top').fadeOut();
+
+  // back to top button
+  $(window).scroll(function () {
+    // oonly appears when the scroll is > 100px
+    if ($(this).scrollTop() > 100) {
+      $('#back-to-top').fadeIn();
+    } else {
+      $('#back-to-top').fadeOut();
+    }
+  });
+
+  $('#back-to-top').click(function () {
+    $('#body,html').animate({
+      scrollTop: 0
+    }, 700);
+    return false;
   });
 });
